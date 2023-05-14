9a247166066be81a3d31af031ebbcab11746d376
kabrownie <makonokevin@gmail.com>
2023-05-14 15:19:32 +0300

    signup

---
+++ app/src/main/java/com/dekut/sales/signup.java
@@ -0,0 +1,136 @@
+package com.dekut.sales;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class signup extends AppCompatActivity {
+    // Initialize variables
+   private FirebaseAuth mAuth;
+    private EditText email, password, name;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        email = findViewById(R.id.register_email);
+        name = findViewById(R.id.register_name);
+        password = findViewById(R.id.register_password);
+        Button mRegister = findViewById(R.id.register_button);
+        TextView existaccount = findViewById(R.id.homepage);
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Register");
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emaill = email.getText().toString().trim();
+                String uname = name.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(emaill).matches()) {
+                    email.setError("Invalid Email");
+                    email.setFocusable(true);
+                } else if (pass.length() < 6) {
+                    password.setError("Length Must be greater than 6 character");
+                    password.setFocusable(true);
+                }
+                else {
+                    registerUser(emaill, pass, uname);
+                }
+            }
+        });
+        existaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signup.this, Login.class));
+            }
+        });
+    }
+
+    private void registerUser(String emaill, final String pass, final String uname) {
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(emaill, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String email = user.getEmail();
+                    String uid = user.getUid();
+                    HashMap<Object, String> hashMap = new HashMap<>();
+                    hashMap.put("email", email);
+                    hashMap.put("uid", uid);
+                    hashMap.put("name", uname);
+                    hashMap.put("onlineStatus", "online");
+                    hashMap.put("typingTo", "noOne");
+                    hashMap.put("image", "");
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("Users");
+                    reference.child(uid).setValue(hashMap);
+                    Toast.makeText(signup.this, "Registered User " + user.getEmail(), Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(signup.this, dashboard.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();}
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "User with Email id already exists",
+                                    Toast.LENGTH_LONG).show();
+                    }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(signup.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(signup.this, "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        ///sign in with google>>
+        // Assign variable
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();}
+}
